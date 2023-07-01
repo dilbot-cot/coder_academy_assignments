cacea1164f28a8d16fbec2385e4d1884519c4faa
dilbot-cot <dilloncotter@icloud.com>
2023-07-01 14:00:11 +1000

    finished initial build for term range

--- dilloncotter_T1A3/src/input.py
+++ dilloncotter_T1A3/src/input.py
@@ -30,3 +30,17 @@ class UserInput:
                     print("Invalid selection. Please enter either 1, 2, or 3.")
             except ValueError:
                 print("Invalid input. Please enter a number.") #handles error where a non numerical value is input
+
+    # function to get the loan term range
+    def get_term_range(self):
+        while True:
+            try:
+                #ask for the loan term range
+                term_start = int(input("Enter the start of the loan term you would like to test (in years): "))
+                term_end = int(input("Enter the end of the loan term you would like to test (in years): "))
+                if term_start > 0 and term_end > 0 and term_end >= term_start:
+                    return term_start, term_end
+                else:
+                    print("Inavlid range. Both terms must be positive and term end must be larger or equal to term start")
+            except ValueError:
+                print("Invalid input. Please enter a number.") #handles error where a non numerical value is input
