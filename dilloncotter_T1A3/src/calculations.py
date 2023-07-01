4df2c1e3b40f493eac019d6f5253249fb1dc0ab2
dilbot-cot <dilloncotter@icloud.com>
2023-07-01 14:31:22 +1000

    first build complete?

--- dilloncotter_T1A3/src/calculations.py
+++ dilloncotter_T1A3/src/calculations.py
@@ -4,8 +4,7 @@ class Loan:
         self.loan_amount = loan_amount
         self.num_payments = num_payments
 
-    # Calculates and returns the laon repayment for a specific loan term and interest rate
-    def calculate_loan_repayments(self, loan_term, interest_rate):
-        rate = (interest_rate / 100) / 12 #interest is charged monthly
-        repayments = loan_term * self.num_payments
+    def calculate_loan_repayments(self):
+        rate = self.interest_rate / (100 * self.num_payments) #interest is charged per repayment period
+        repayments = self.loan_term * self.num_payments
         return (rate * self.loan_amount) / (1 - (1 + rate) ** -repayments)
--- dilloncotter_T1A3/src/main.py
+++ dilloncotter_T1A3/src/main.py
@@ -2,17 +2,27 @@ from input import UserInput
 from calculations import Loan
 
 def main():
-    # initialises UserInput and Loan in the main package
     user_input = UserInput()
-    loan_calculator = Loan()
-# get all user inputs
+
     loan_amount = user_input.get_loan_amount()
     frequency = user_input.get_repayment_frequency()
     term_start, term_end = user_input.get_term_range()
     rate_start, rate_end = user_input.get_interest_rate_range()
 
-    #complete loop over rate and term
+    # Map the frequencies to the number of repayments per year
+    frequencies = {
+        "weekly": 52,
+        "fortnightly": 26,
+        "monthly": 12
+    }
+
     for term in range(term_start, term_end + 1):
         for rate in range(int(rate_start * 100), int(rate_end * 100) + 1, 25):
-            repayments = loan_calculator.calculate_loan_repayments(loan_amount, frequency, term, rate / 100)
-            print(f"For a loan term of {term} years and an interest rate of {rate / 100}%, your repayments would be ${repayments:.2f} per {frequency}.")
+            loan_calculator = Loan(loan_amount, frequencies[frequency])
+            loan_calculator.loan_term = term
+            loan_calculator.interest_rate = rate / 100
+            repayments = loan_calculator.calculate_loan_repayments()
+            print(f"For a loan term of {term} years and an interest rate of {rate / 100}%, your repayments would be ${repayments:.2f} {frequency}.")
+
+if __name__ == "__main__":
+    main()
