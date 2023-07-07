cb7737cfcdf4915be2b01af4e964a4b86014ade8
dilbot-cot <dilloncotter@icloud.com>
2023-07-08 08:39:43 +1000

    fixed testing code

--- dilloncotter_T1A3/src/test_caluclations.py
+++ dilloncotter_T1A3/src/test_caluclations.py
@@ -1,16 +1,15 @@
-from calculations import Loan
+from calculations import RepaymentCalculator
+from loan_details import LoanDetails
 
-def test_loan_repayments():
+def test_calculate_loan_repayments():
     # Testing with a known loan amount, term and interest rate.
     
     # Test 1: 5 year term with 5% interest
-    loan_1 = Loan(10000, 12)
-    loan_1.loan_term = 5
-    loan_1.interest_rate = 5
+    loan_details_1 = LoanDetails(10000, 12, 5, 5)
+    loan_1 = RepaymentCalculator(loan_details_1)
     assert abs(loan_1.calculate_loan_repayments() - 188.71) < 0.01
 
     # Test 2: 10 year term with 5% interest
-    loan_2 = Loan(10000, 12)
-    loan_2.loan_term = 10
-    loan_2.interest_rate = 5
+    loan_details_2 = LoanDetails(10000, 12, 10, 5)
+    loan_2 = RepaymentCalculator(loan_details_2)
     assert abs(loan_2.calculate_loan_repayments() - 106.07) < 0.01
