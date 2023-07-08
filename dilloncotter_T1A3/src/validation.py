2390a6ae7e7d104be3b1d47099cdc1206bb50dfb
dilbot-cot <dilloncotter@icloud.com>
2023-07-08 10:45:23 +1000

    Packaging for testing on separate machine

--- dilloncotter_T1A3/src/validation.py
+++ dilloncotter_T1A3/src/validation.py
@@ -1,5 +1,8 @@
 class Validation:
+    #Function to validate the loan amount
+    #This is used for testing
     @staticmethod
+    #staticmethod allows us to call the validate_loan_amount function without creating it as an object
     def validate_loan_amount(loan_amount):
         try:
             loan_amount = float(loan_amount)
