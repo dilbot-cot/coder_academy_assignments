ed3a8053d23fc44aeed1126708e732d7011fbeca
dilbot-cot <dilloncotter@icloud.com>
2023-07-12 09:00:22 +1000

    Updated testing script after altering code

--- dilloncotter_T1A3/src/test_input.py
+++ dilloncotter_T1A3/src/test_input.py
@@ -1,17 +1,17 @@
-from input import UserInput
+from validation import Validation
 
 def test_loan_amount_validation():
     # Testing the loan amount validation
-    user_input = UserInput()
+    validation = Validation()
 
     # Test case 1: Valid loan amount
-    valid, value = user_input.validate_loan_amount("10000")
+    valid, value = validation.validate_loan_amount("10000")
     assert valid and value == 10000  # Expect True and 10000
 
     # Test case 2: Invalid loan amount (negative)
-    valid, value = user_input.validate_loan_amount("-10000")
+    valid, value = validation.validate_loan_amount("-10000")
     assert not valid and value is None  # Expect False and None
 
     # Test case 3: Invalid loan amount (non-numeric)
-    valid, value = user_input.validate_loan_amount("abc")
+    valid, value = validation.validate_loan_amount("abc")
     assert not valid and value is None  # Expect False and None
