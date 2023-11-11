2a869a7752232bdb1d9c43095573fb76f5e280ae
dilbot-cot <dilloncotter@icloud.com>
2023-11-11 11:03:57 +1100

    get picture URL function complete

--- term3/imagedownloader/downloader.js
+++ term3/imagedownloader/downloader.js
@@ -15,21 +15,33 @@ const {finished} = require("node:stream/promises");
 // ./folder/subfolder/filename.extension
 const path = require("node:path")
 
-const API_BASE_URL = "https://pokeapi.co/api/v2/pokemon/";
-
-function downloadPokemonPicture(targetId = getRandomPokemonId()){
-
-}
+const API_URL_BASE = "https://pokeapi.co/api/v2/pokemon/";
 
 // return random number 1-1017
 function getRandomPokemonId() {
-    return Math.floor(Math.random() * 1017)+1
+    return Math.floor(Math.random() * 1017) + 1;
 }
 
 // Get Pokemon data for the id
 // Get image url from that data
 async function getPokemonPictureUrl(targetId = getRandomPokemonId()){
+    let response = await fetch(API_URL_BASE + targetId).catch(error => {
+        throw new Error("API failure");
+    });
 
+    if (response.status == "404") {
+        throw new Error("API did not have data for the request ID");
+    };
+
+    let data = await response.json().catch(error => {
+        throw new Error("API did not return valid JSON");
+    });
+
+    return data.sprites.other["official-artowrk"].front_default;
+}
+
+function downloadPokemonPicture(targetId = getRandomPokemonId()){
+    
 }
 
 // Download the image and save to computer
