5d06a53dd414098664c163f3e22a8f4475e6894e
dilbot-cot <dilloncotter@icloud.com>
2023-11-14 20:03:02 +1100

    first break

--- term3/may23-t3w5-reactjs/src/App.js
+++ term3/may23-t3w5-reactjs/src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
 
+// Array of reuit names:
+
+let someFruit = [
+  "mangoes",
+  "pear",
+  "oranges",
+  "strawberries",
+  "bananas"
+]
+
 function App() {
   return (
     <div className="App">
-      <h1>Working on our own now</h1>
+      <h1>The Fruit App!</h1>
+      <ul>
+        {
+          someFruit.map((fruit => {
+            return <li>{fruit}</li>
+          }))
+        }
+      </ul>
     </div>
   );
 }
